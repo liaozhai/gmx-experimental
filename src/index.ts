a248338973c746be945d58d884309982ff55ff5d
liaozhai <laocan@yandex.ru>
2020-10-13 13:32:36 +0300

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,5 @@
-// import Controller from './Controller';
-// import Evented from './Evented';
-// import View from './View';
-// import './index.css';
-// import Application from './Application';
-// import ViewManager from './ViewManager';
-// import Layer from './Layer';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
-// class DefaultView extends Evented implements View {    
-//     private _name: string;
-//     constructor (name: string) {
-//         super();
-//         this._name = name;        
-//     }    
-//     open(container: HTMLElement, model: any) {
-//         container.innerHTML = '';        
-//     }
-//     close() {
-        
-//     }
-// }
-
-// class DefaultController extends Controller {
-//     constructor(name:string) {
-//         super(name);
-//     }
-// }
 
 window.addEventListener('load', () => {
     let map = L.map(document.body);
